b8b1ebdb4e988f93df3495de55d34d5360322fa2
chensw <254829127@qq.com>
2017-08-24 21:24:01 +0800

    允许自动旋转

--- ZFPlayer/Category/UIViewController+ZFPlayerRotation.m
+++ ZFPlayer/Category/UIViewController+ZFPlayerRotation.m
@@ -31,12 +31,16 @@
 
 // 是否支持自动转屏
 - (BOOL)shouldAutorotate {
-    return NO;
+    
+    return YES;
+    
 }
 
 // 支持哪些屏幕方向
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskPortrait;
+    
+    return UIInterfaceOrientationMaskAllButUpsideDown;
+    
 }
 
 // 默认的屏幕方向（当前ViewController必须是通过模态出来的UIViewController（模态带导航的无效）方式展现出来的，才会调用这个方法）
